a5d87f087d8814c5b49503960c98d4b0f22fdbe1
super9du <super.9du@gmail.com>
2022-08-19 22:00:13 +0800

    feat: 新增批量发送API

--- src/main/java/super9du/sia/aliyun/sms/AliyunSmsTemplate.java
+++ src/main/java/super9du/sia/aliyun/sms/AliyunSmsTemplate.java
@@ -1,17 +1,19 @@
 /*
  * aliyun-sms-spring-boot-starter
  * Copyright 2022  super9du
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package super9du.sia.aliyun.sms;
 
 import com.aliyun.dysmsapi20170525.Client;
+import com.aliyun.dysmsapi20170525.models.SendBatchSmsRequest;
+import com.aliyun.dysmsapi20170525.models.SendBatchSmsResponse;
 import com.aliyun.dysmsapi20170525.models.SendSmsRequest;
 import com.aliyun.dysmsapi20170525.models.SendSmsResponse;
 import lombok.Getter;
@@ -42,7 +44,7 @@ public class AliyunSmsTemplate {
      *
      * @param templateCode 阿里云「短信模板编号」
      * @param phoneNumber  发送的手机号
-     * @return SendSmsResponse
+     * @return {@link SendSmsRequest}
      */
     public SendSmsRequest createSendSmsRequest(String templateCode, String phoneNumber) {
         return new SendSmsRequest()
@@ -50,5 +52,54 @@ public class AliyunSmsTemplate {
                 .setTemplateCode(templateCode)
                 .setTemplateParam(aliyunSmsProperties.getTemplateParam(templateCode))
                 .setPhoneNumbers(phoneNumber);
+    }
+
+    /**
+     * 批量发送短信
+     * <p>
+     * <strong>注意</strong>：signatureList、phoneNumberList、templateParamList 三个参数的长度相等，
+     * 且应该是一一对应的。即 signatureList[0]、phoneNumberList[0]、templateParamList[0]
+     * 三个一组确定一个短信由谁发给谁，发送什么内容。
+     *
+     * @param templateCode      阿里云「短信模板编号」
+     * @param signatureList     阿里云「短信签名」列表
+     * @param phoneNumberList   手机号的列表
+     * @param templateParamList 阿里云「短信模板变量」的列表
+     * @return {@link SendBatchSmsRequest}
+     */
+    public SendBatchSmsResponse sendBatch(
+            String templateCode,
+            Iterable<CharSequence> signatureList,
+            Iterable<CharSequence> phoneNumberList,
+            Iterable<CharSequence> templateParamList
+    ) throws Exception {
+        return client.sendBatchSms(createSendBatchSmsRequest(
+                templateCode, signatureList, phoneNumberList, templateParamList));
+    }
+
+    /**
+     * 根据指定参数生成 {@link SendBatchSmsRequest}
+     * <p>
+     * <strong>注意</strong>：signatureList、phoneNumberList、templateParamList 三个参数的长度相等，
+     * 且应该是一一对应的。即 signatureList[0]、phoneNumberList[0]、templateParamList[0]
+     * 三个一组确定一个短信由谁发给谁，发送什么内容。
+     *
+     * @param templateCode      阿里云「短信模板编号」
+     * @param signatureList     阿里云「短信签名」列表
+     * @param phoneNumberList   手机号的列表
+     * @param templateParamList 阿里云「短信模板变量」的列表
+     * @return {@link SendBatchSmsRequest}
+     */
+    public SendBatchSmsRequest createSendBatchSmsRequest(
+            String templateCode,
+            Iterable<CharSequence> signatureList,
+            Iterable<CharSequence> phoneNumberList,
+            Iterable<CharSequence> templateParamList
+    ) {
+        return new SendBatchSmsRequest()
+                .setSignNameJson(Utils.toJsonList(signatureList))
+                .setPhoneNumberJson(Utils.toJsonList(phoneNumberList))
+                .setTemplateCode(templateCode)
+                .setTemplateParamJson(Utils.toJsonList(templateParamList));
     }
 }
